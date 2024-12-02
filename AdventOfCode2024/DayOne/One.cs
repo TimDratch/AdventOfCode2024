252583830f9b7419b15d4f83683305a0865b167c
Timothy Dratch <tdratch@printscan.com>
2024-12-02 15:13:06 -0500

    Add project files.

---
+++ AdventOfCode2024/DayOne/One.cs
@@ -0,0 +1,49 @@
+﻿
+namespace AdventOfCode2024.DayOne
+{
+    public class One
+    {
+        private static readonly List<int> ListA = [];
+        private static readonly List<int> ListB = [];
+        private static int Count = 0;
+
+         public static void PopulateInput()
+        {
+            var data = File.ReadAllLines(@"./Dayone/Input/Input.txt");
+            foreach (var line in data)
+            {
+                var split = line.Split(' ')
+                    .Where(x => x is not null)
+                    .ToArray();
+                ListA.Add(int.Parse(split.First()));
+                ListB.Add(int.Parse(split.Last()));
+            }
+            Count = ListA.Count;
+        }
+
+        public static int CalculatePartOne()
+        {
+            var sum = 0;
+
+            var orderedA = ListA.OrderBy(x => x).ToArray();
+            var orderedB = ListB.OrderBy(x => x).ToArray();
+            for (var i = 0; i < Count; i++)
+            {
+                sum += GetDifference(orderedA[i], orderedB[i]);
+            }
+
+            return sum;
+        }
+
+        public static int CalculatePartTwo()
+        {
+            return ListA.Sum(x => x * ListB.Where(y => y == x).Count());
+        }
+
+        private static int GetDifference(int a, int b)
+        {
+            return a > b ? a - b : b - a;
+        }
+
+    }
+}
---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,14 @@
+﻿using AdventOfCode2024.DayOne;
+
+namespace AdventOfCode2024
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            One.PopulateInput();
+            Console.WriteLine("Part One: " + One.CalculatePartOne());
+            Console.WriteLine("Part Two: " + One.CalculatePartTwo());
+        }
+    }
+}
