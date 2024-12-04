827d9e845338dbb63243c0afb6bfe68fb940f2a2
Timothy Dratch <tdratch@printscan.com>
2024-12-04 10:55:19 -0500

    Day 2

---
+++ AdventOfCode2024/DayTwo/Two.cs
@@ -0,0 +1,79 @@
+﻿
+namespace AdventOfCode2024.DayTwo
+{
+    public class Two
+    {
+        private static List<int[]> Data = [];
+        private static readonly char[] separator = [' '];
+
+        public static void PopulateInput()
+        {
+            var data = File.ReadAllLines(@"./DayTwo/Input/Input.txt");
+            var split = data.Select(x => x.Split(separator, StringSplitOptions.RemoveEmptyEntries).ToArray());
+            Data = split.Select(x => Array.ConvertAll(x, int.Parse)).ToList();
+        }
+
+        public static int CalculatePartOne()
+        {
+            var count = 0;
+
+            foreach (var line in Data)
+                if (ValidateIncrementation(line, false)) count++;
+
+            return count;
+        }
+        public static int CalculatePartTwo()
+        {
+            var count = 0;
+
+            foreach (var line in Data)
+                if (ValidateIncrementation(line, true)) count++;
+
+            return count;
+        }
+
+        private static bool ValidateIncrementation(int[] arr, bool shouldRetry)
+        {
+            bool? shouldBeAsc = null;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i == 0)
+                    continue;
+
+                var difference = arr[i] - arr[i - 1];
+
+                switch (shouldBeAsc)
+                {
+                    case null:
+                        shouldBeAsc = difference > 0;
+                        break;
+                    case true:
+                        if (difference < 0)
+                            return shouldRetry && TrimLevelAndRetry(arr);
+                        break;
+                    case false:
+                        if (difference > 0)
+                            return shouldRetry && TrimLevelAndRetry(arr);
+                        break;
+                }
+
+                var abs = Math.Abs(difference);
+                if (abs < 1 || abs > 3)
+                    return shouldRetry && TrimLevelAndRetry(arr);
+            }
+            return true;
+        }
+
+        private static bool TrimLevelAndRetry(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var trimmedArr = arr.Where((x, y) => y != i).ToArray();
+
+                if (ValidateIncrementation(trimmedArr, false) == true)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -1,4 +1,5 @@
 ﻿using AdventOfCode2024.DayOne;
+using AdventOfCode2024.DayTwo;
 
 namespace AdventOfCode2024
 {
@@ -7,8 +8,13 @@ namespace AdventOfCode2024
         public static void Main()
         {
             One.PopulateInput();
+            Console.WriteLine("Day One");
             Console.WriteLine("Part One: " + One.CalculatePartOne());
             Console.WriteLine("Part Two: " + One.CalculatePartTwo());
+            Two.PopulateInput();
+            Console.WriteLine("Day Two");
+            Console.WriteLine("Part One: " + Two.CalculatePartOne());
+            Console.WriteLine("Part Two: " + Two.CalculatePartTwo());
         }
     }
 }
